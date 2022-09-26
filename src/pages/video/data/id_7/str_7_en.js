b012e6920335db4a9ab509d11cdba6361ad0a867
Vitaliy Zarubin <zarubin@surf.dev>
2022-09-26 23:04:31 +0300

    Fix str

--- src/pages/video/data/id_7/str_7_en.js
+++ src/pages/video/data/id_7/str_7_en.js
@@ -1,21 +1,12 @@
 export const str_7_en = `
 1
-00:00:00,270 --> 00:00:04,470
+00:00:00,270 --> 00:00:13,510
 - Hey, if you work in a
 company, in a team in a company
-
-2
-00:00:04,470 --> 00:00:07,820
 and you are at least
 a mid-level developer,
-
-3
-00:00:07,820 --> 00:00:10,350
 then once in a while,
 your team will ask you
-
-4
-00:00:10,350 --> 00:00:13,510
 to interview new people who
 would like to join the team.
 
@@ -24,51 +15,30 @@ would like to join the team.
 So you will be the interviewer.
 
 6
-00:00:15,130 --> 00:00:18,583
+00:00:15,130 --> 00:00:23,940
 I play this role a few times
 a week that's for sure,
-
-7
-00:00:19,820 --> 00:00:22,100
 and there is a problem with the interviews
-
-8
-00:00:22,100 --> 00:00:23,940
 when you are an active interviewer.
 
 9
-00:00:23,940 --> 00:00:25,560
+00:00:23,940 --> 00:00:27,963
 Actually, the problem is even bigger
-
-10
-00:00:25,560 --> 00:00:27,963
 when you are not a very
 active interviewer.
 
 11
-00:00:28,930 --> 00:00:33,110
+00:00:28,930 --> 00:00:36,240
 The problem is how do
 you explain your opinion
-
-12
-00:00:33,110 --> 00:00:36,240
 about the person you interview
 to the rest of the team?
 
 13
-00:00:36,240 --> 00:00:37,500
+00:00:36,240 --> 00:00:42,300
 They will give you the programmer,
-
-14
-00:00:37,500 --> 00:00:39,500
 they will ask you to interview that guy
-
-15
-00:00:39,500 --> 00:00:40,950
 and then after the interview,
-
-16
-00:00:40,950 --> 00:00:42,300
 they will ask you what do you think?
 
 17
@@ -81,33 +51,21 @@ You have to summarize your
 experience after the interview.
 
 19
-00:00:48,800 --> 00:00:51,050
+00:00:48,800 --> 00:00:56,780
 And what's more interesting is that
-
-20
-00:00:51,050 --> 00:00:53,920
 they will sometimes ask you
-
-21
-00:00:53,920 --> 00:00:56,780
 to give that summary after a while.
 
 22
-00:00:56,780 --> 00:00:58,470
+00:00:56,780 --> 00:01:01,480
 They will ask you right
 after the interview,
-
-23
-00:00:58,470 --> 00:01:01,480
 but maybe after a while as
 well, maybe in a few weeks even.
 
 24
-00:01:01,480 --> 00:01:03,210
+00:01:01,480 --> 00:01:04,129
 And it's hard, it's hard to remember
-
-25
-00:01:03,210 --> 00:01:04,129
 the people you interview.
 
 26
@@ -120,51 +78,33 @@ why you give the opinion.
 So I share my recipe how I do it.
 
 28
-00:01:11,780 --> 00:01:14,750
+00:01:11,780 --> 00:01:18,520
 I design a very small file for myself,
-
-29
-00:01:14,750 --> 00:01:18,520
 a very small template of
 the most important questions
 
 30
-00:01:18,520 --> 00:01:21,420
+00:01:18,520 --> 00:01:22,960
 I ask people who I interview,
-
-31
-00:01:21,420 --> 00:01:22,960
 and they're not technical questions.
 
 32
-00:01:22,960 --> 00:01:26,820
+00:01:22,960 --> 00:01:34,760
 I don't really specify the
 exact Java questions I may ask,
-
-33
-00:01:26,820 --> 00:01:30,620
 but I broke down all the aspects
-
-34
-00:01:30,620 --> 00:01:34,760
 of questions I ask that guy.
 
 35
-00:01:34,760 --> 00:01:39,347
+00:01:34,760 --> 00:01:45,670
 And then I approximately
 defined the ranges
-
-36
-00:01:41,040 --> 00:01:45,670
 in which I measure the answers I hear.
 
 37
-00:01:45,670 --> 00:01:49,470
+00:01:45,670 --> 00:01:50,670
 For example, I pay attention
 to the open source history
-
-38
-00:01:49,470 --> 00:01:50,670
 of the people who I interview.
 
 39
@@ -172,21 +112,15 @@ of the people who I interview.
 So it does matter for me.
 
 40
-00:01:52,200 --> 00:01:53,880
+00:01:52,200 --> 00:01:56,700
 When you come to me for the interview,
-
-41
-00:01:53,880 --> 00:01:56,700
 I will ask you what's your
 experience in open source?
 
 42
-00:01:56,700 --> 00:01:59,300
+00:01:56,700 --> 00:02:01,480
 Did you try to commit to
 some open source projects,
-
-43
-00:01:59,300 --> 00:02:01,480
 or maybe you have your
 own open source projects?
 
@@ -204,16 +138,10 @@ Then I give you zero in
 this particular line.
 
 47
-00:02:08,330 --> 00:02:11,039
+00:02:08,330 --> 00:02:16,620
 If you tell me that you
 are an active contributor
-
-48
-00:02:11,039 --> 00:02:13,520
 to a few large open source products
-
-49
-00:02:13,520 --> 00:02:16,620
 and you have your own stuff,
 then I will give you seven, 10.
 
@@ -222,16 +150,10 @@ then I will give you seven, 10.
 My range is from zero to 10.
 
 51
-00:02:18,780 --> 00:02:21,920
+00:02:18,780 --> 00:02:26,080
 And then I quickly in just one sentence,
-
-52
-00:02:21,920 --> 00:02:24,800
 explain to myself why
 I gave you that remark,
-
-53
-00:02:24,800 --> 00:02:26,080
 why I gave you five.
 
 54
@@ -247,70 +169,40 @@ And I have other things like education.
 I pay attention to where you graduated.
 
 57
-00:02:33,250 --> 00:02:36,440
+00:02:33,250 --> 00:02:40,720
 One thing is your bachelor,
 or maybe no education
-
-58
-00:02:36,440 --> 00:02:40,720
 in no university, or maybe
 you get a PhD in Stanford.
 
 59
-00:02:40,720 --> 00:02:44,070
+00:02:40,720 --> 00:02:46,123
 So on the range from zero to
 10, I will give you the mark
-
-60
-00:02:44,070 --> 00:02:46,123
 and I'll put some words there.
 
 61
-00:02:46,980 --> 00:02:48,420
+00:02:46,980 --> 00:03:02,040
 Another thing I pay attention to
-
-62
-00:02:48,420 --> 00:02:51,152
 is I usually give to Java
 programmers or Ruby programmers
-
-63
-00:02:51,152 --> 00:02:56,090
 and to C++ programmers, I give
 a very small piece of code
-
-64
-00:02:56,090 --> 00:02:59,990
 and I ask the people I
 interview to find bugs,
-
-65
-00:02:59,990 --> 00:03:02,040
 to find problems in this piece of code.
 
 66
-00:03:02,040 --> 00:03:03,370
+00:03:02,040 --> 00:03:09,890
 And according to their answers,
-
-67
-00:03:03,370 --> 00:03:06,030
 I make my judgment during the interview
-
-68
-00:03:06,030 --> 00:03:07,730
 how good or bad they are,
-
-69
-00:03:07,730 --> 00:03:09,890
 and sometimes it's not about good or bad.
 
 70
-00:03:09,890 --> 00:03:12,130
+00:03:09,890 --> 00:03:14,460
 Actually, often it's
 not about good or bad,
-
-71
-00:03:12,130 --> 00:03:14,460
 it's about what kind of bugs they find.
 
 72
@@ -323,31 +215,22 @@ Some people see high level
 bugs, design problems there.
 
 74
-00:03:20,210 --> 00:03:22,910
+00:03:20,210 --> 00:03:25,380
 So it's not about actually
 judging on your level,
-
-75
-00:03:22,910 --> 00:03:25,380
 but about judging what
 you pay attention to.
 
 76
-00:03:25,380 --> 00:03:28,300
+00:03:25,380 --> 00:03:30,400
 Like, what are you a designer,
 or you're the programmer,
-
-77
-00:03:28,300 --> 00:03:30,400
 or maybe the architect?
 
 78
-00:03:30,400 --> 00:03:34,720
+00:03:30,400 --> 00:03:37,360
 So I write down my judgment,
 I write down my conclusions,
-
-79
-00:03:34,720 --> 00:03:37,360
 and I give you the number from zero to 10.
 
 80
@@ -363,24 +246,15 @@ It's a simple one page document.
 I put some details there.
 
 83
-00:03:42,100 --> 00:03:43,830
+00:03:42,100 --> 00:03:48,310
 The date of the interview,
-
-84
-00:03:43,830 --> 00:03:48,310
 what kind of general
 information I received from you.
 
 85
-00:03:48,310 --> 00:03:49,610
+00:03:48,310 --> 00:03:55,240
 Very short, very brief,
-
-86
-00:03:49,610 --> 00:03:52,040
 and I put this document to my computer,
-
-87
-00:03:52,040 --> 00:03:55,240
 and I have these documents
 for the last years.
 
@@ -390,31 +264,16 @@ I can go back a few years
 back and I can open.
 
 89
-00:03:58,380 --> 00:04:00,260
+00:03:58,380 --> 00:04:13,850
 For example, I interviewed
 you a few years ago
-
-90
-00:04:00,260 --> 00:04:02,900
 you come back to me again,
 then I open the file
-
-91
-00:04:02,900 --> 00:04:05,310
 and I know what kind of
 questions I was asking you,
-
-92
-00:04:05,310 --> 00:04:06,660
 what kind of answers I got,
-
-93
-00:04:06,660 --> 00:04:11,130
 and why I decided that
 your level is five here,
-
-94
-00:04:11,130 --> 00:04:13,850
 three here, seven here,
 and so on and so forth.
 
@@ -423,26 +282,17 @@ and so on and so forth.
 I suggest you do the same.
 
 96
-00:04:15,380 --> 00:04:19,320
+00:04:15,380 --> 00:04:28,310
 Define for yourself the
 things you pay attention to,
-
-97
-00:04:19,320 --> 00:04:24,320
 define the ranges, define the
 way you measure the answers,
-
-98
-00:04:24,430 --> 00:04:28,310
 and make this template the same
 for everybody you interview.
 
 99
-00:04:28,310 --> 00:04:30,440
+00:04:28,310 --> 00:04:32,320
 And don't expect your company or your team
-
-100
-00:04:30,440 --> 00:04:32,320
 to provide this template to you.
 
 101
@@ -450,88 +300,52 @@ to provide this template to you.
 Sometimes they would.
 
 102
-00:04:33,810 --> 00:04:35,970
+00:04:33,810 --> 00:04:42,000
 The large companies
 they usually expect you
-
-103
-00:04:35,970 --> 00:04:39,350
 to somehow structure the
 result of the interview,
-
-104
-00:04:39,350 --> 00:04:42,000
 somehow structure your conclusion.
 
 105
-00:04:42,000 --> 00:04:45,160
+00:04:42,000 --> 00:04:51,000
 But I would suggest to
 still do your own work
-
-106
-00:04:45,160 --> 00:04:46,780
 and define your own template,
-
-107
-00:04:46,780 --> 00:04:49,610
 your own way of how you interview people,
-
-108
-00:04:49,610 --> 00:04:51,000
 what you pay attention to.
 
 109
-00:04:51,000 --> 00:04:52,410
+00:04:51,000 --> 00:04:55,140
 And stick to this template
-
-110
-00:04:52,410 --> 00:04:55,140
 through the years of interviews you do.
 
 111
-00:04:55,140 --> 00:04:57,520
+00:04:55,140 --> 00:05:00,870
 In this case, it'll be
 easier for you to compare
-
-112
-00:04:57,520 --> 00:05:00,870
 a candidate to candidate
 because you can see the numbers.
 
 113
-00:05:00,870 --> 00:05:02,180
+00:05:00,870 --> 00:05:11,280
 Even though people are different,
-
-114
-00:05:02,180 --> 00:05:04,060
 of course it's hard to
 compare them by the numbers,
-
-115
-00:05:04,060 --> 00:05:07,440
 but still you will be able
 to explain to your team
-
-116
-00:05:07,440 --> 00:05:11,280
 why this candidate is a go for
 you, and this one is no go.
 
 117
-00:05:11,280 --> 00:05:13,300
+00:05:11,280 --> 00:05:18,300
 So this explanation will be based
-
-118
-00:05:13,300 --> 00:05:18,300
 on some objective information,
 objective measurements.
 
 119
-00:05:18,390 --> 00:05:19,810
+00:05:18,390 --> 00:05:22,910
 Of course, they're not really objective,
-
-120
-00:05:19,810 --> 00:05:22,910
 but still it's better than
 two subjective opinions.
 
@@ -549,15 +363,9 @@ It kinda feels like you're
 not really professional.
 
 124
-00:05:29,040 --> 00:05:31,160
+00:05:29,040 --> 00:05:37,360
 But if you have a document for this one
-
-125
-00:05:31,160 --> 00:05:33,310
 and a document for that one, very short,
-
-126
-00:05:33,310 --> 00:05:37,360
 still it's better to
 explain your decision.
 
@@ -572,5 +380,4 @@ Thanks for listening.
 129
 00:05:39,453 --> 00:05:40,733
 Stay tuned. Bye bye.
-
 `
--- src/pages/video/data/id_9/str_9_en.js
+++ src/pages/video/data/id_9/str_9_en.js
@@ -1,55 +1,31 @@
 export const str_9_en = `
 1
-00:00:00,120 --> 00:00:03,150
+00:00:00,120 --> 00:00:06,360
 - Hey, usually I share
 with you my opinion,
-
-2
-00:00:03,150 --> 00:00:04,380
 but this time I would like to
-
-3
-00:00:04,380 --> 00:00:06,360
 ask you for your opinion.
 
 4
-00:00:06,360 --> 00:00:08,420
+00:00:06,360 --> 00:00:11,490
 What would you do in, in this situation
-
-5
-00:00:08,420 --> 00:00:09,253
 I will describe you
-
-6
-00:00:09,253 --> 00:00:11,490
 Or how would you configure the situation?
 
 7
-00:00:11,490 --> 00:00:13,350
+00:00:11,490 --> 00:00:17,470
 Imagine there is a research team,
-
-8
-00:00:13,350 --> 00:00:16,160
 a group of programmers/researchers
-
-9
-00:00:16,160 --> 00:00:17,470
 who are doing some experiments.
 
 10
-00:00:17,470 --> 00:00:20,520
+00:00:17,470 --> 00:00:22,000
 They're trying to solve some problems,
-
-11
-00:00:20,520 --> 00:00:22,000
 not problem, problems usually.
 
 12
-00:00:22,000 --> 00:00:23,770
+00:00:22,000 --> 00:00:26,730
 They're all looking at these problems
-
-13
-00:00:23,770 --> 00:00:26,730
 from different perspectives,
 from different angles.
 
@@ -58,33 +34,18 @@ from different angles.
 They're, and they are working on papers.
 
 15
-00:00:30,410 --> 00:00:31,530
+00:00:30,410 --> 00:00:39,370
 They're not only writing code
-
-16
-00:00:31,530 --> 00:00:34,470
 but they're also writing
 scientific papers, academic papers
-
-17
-00:00:34,470 --> 00:00:37,260
 which are supposed to be published
-
-18
-00:00:37,260 --> 00:00:39,370
 in the end of the research
 or during the research.
 
 19
-00:00:39,370 --> 00:00:41,030
+00:00:39,370 --> 00:00:44,510
 The papers are usually written
-
-20
-00:00:41,030 --> 00:00:43,160
 by not a single person,
-
-21
-00:00:43,160 --> 00:00:44,510
 but a few people.
 
 22
@@ -102,25 +63,16 @@ Another one is participating
 in the discussions.
 
 25
-00:00:51,900 --> 00:00:54,960
+00:00:51,900 --> 00:00:58,460
 So maybe not even, you
 know, typing the text there
-
-26
-00:00:54,960 --> 00:00:58,460
 but discussing the problems
 and and suggesting.
 
 27
-00:00:58,460 --> 00:00:59,880
+00:00:58,460 --> 00:01:04,730
 Suggesting
-
-28
-00:00:59,880 --> 00:01:00,730
 solutions
-
-29
-00:01:00,730 --> 00:01:04,730
 and how to move the research
 into a better direction.
 
@@ -129,39 +81,21 @@ into a better direction.
 So it's a group work, it's a teamwork.
 
 31
-00:01:07,150 --> 00:01:09,110
+00:01:07,150 --> 00:01:15,960
 But then the result of this teamwork
-
-32
-00:01:09,110 --> 00:01:12,090
 there is a paper which has an author,
-
-33
-00:01:12,090 --> 00:01:14,520
 an author or a number of authors
-
-34
-00:01:14,520 --> 00:01:15,960
 or an author and coauthors.
 
 35
-00:01:15,960 --> 00:01:18,250
+00:01:15,960 --> 00:01:21,250
 But usually now in academic world,
-
-36
-00:01:18,250 --> 00:01:21,250
 they just have a list of authors.
 
 37
-00:01:21,250 --> 00:01:22,240
+00:01:21,250 --> 00:01:24,530
 John,
-
-38
-00:01:22,240 --> 00:01:23,274
 Sarah,
-
-39
-00:01:23,274 --> 00:01:24,530
 Peter.
 
 40
@@ -169,15 +103,9 @@ Peter.
 Three authors.
 
 41
-00:01:25,430 --> 00:01:28,920
+00:01:25,430 --> 00:01:32,950
 So the question is, how do we decide
-
-42
-00:01:28,920 --> 00:01:29,820
 in a group
-
-43
-00:01:30,760 --> 00:01:32,950
 who will be the author?
 
 44
@@ -185,52 +113,31 @@ who will be the author?
 Let's say we have 10 people in the team.
 
 45
-00:01:34,800 --> 00:01:36,435
+00:01:34,800 --> 00:01:38,230
 10 people working
-
-46
-00:01:36,435 --> 00:01:38,230
 on the problem.
 
 47
-00:01:38,230 --> 00:01:40,756
+00:01:38,230 --> 00:01:43,913
 They working on the software solution and
-
-48
-00:01:40,756 --> 00:01:43,913
 they will generate five different papers.
 
 49
-00:01:44,870 --> 00:01:46,710
+00:01:44,870 --> 00:01:48,530
 So there are
-
-50
-00:01:46,710 --> 00:01:48,530
 basically three approaches.
 
 51
-00:01:48,530 --> 00:01:51,229
+00:01:48,530 --> 00:01:53,490
 Approach number one is we all
-
-52
-00:01:51,229 --> 00:01:53,490
 become coauthors.
 
 53
-00:01:53,490 --> 00:01:56,026
+00:01:53,490 --> 00:02:01,080
 10 people, 10 author, no
 matter who is publishing,
-
-54
-00:01:56,026 --> 00:01:57,730
 no matter who is writing,
-
-55
-00:01:57,730 --> 00:01:59,330
 no matter what kind of paper it is,
-
-56
-00:01:59,330 --> 00:02:01,080
 no matter what the paper is about.
 
 57
@@ -252,19 +159,10 @@ And remember, we are
 trying to make a rule.
 
 61
-00:02:13,540 --> 00:02:15,550
+00:02:13,540 --> 00:02:22,960
 So we are trying to decide
-
-62
-00:02:15,550 --> 00:02:17,340
 that it's gonna be happen from now
-
-63
-00:02:17,340 --> 00:02:20,390
 till the very end of our
-
-64
-00:02:20,390 --> 00:02:22,960
 the end of a lifetime
 of this, of this group.
 
@@ -282,20 +180,11 @@ So everybody knows what is
 the rule, what to expect.
 
 68
-00:02:28,480 --> 00:02:32,420
+00:02:28,480 --> 00:02:40,420
 Approach number two, the
 person who is responsible,
-
-69
-00:02:32,420 --> 00:02:34,960
 the main author, the main person
-
-70
-00:02:34,960 --> 00:02:36,799
 who is actually typing most of the text,
-
-71
-00:02:36,799 --> 00:02:40,420
 who is driving the paper
 forward, becomes the author.
 
@@ -304,11 +193,8 @@ forward, becomes the author.
 And we always assign that person.
 
 73
-00:02:42,230 --> 00:02:44,010
+00:02:42,230 --> 00:02:46,030
 So when we start writing a new paper,
-
-74
-00:02:44,010 --> 00:02:46,030
 we say, okay now it's your paper.
 
 75
@@ -325,15 +211,9 @@ paper, make it your paper.
 You will publish.
 
 78
-00:02:51,330 --> 00:02:52,600
+00:02:51,330 --> 00:02:55,680
 And Sarah
-
-79
-00:02:52,600 --> 00:02:53,523
 becomes
-
-80
-00:02:53,523 --> 00:02:55,680
 the author number one.
 
 81
@@ -378,11 +258,8 @@ author of the paper.
 Even if this decision is not fair.
 
 90
-00:03:19,640 --> 00:03:21,070
+00:03:19,640 --> 00:03:22,810
 Maybe the author will just say
-
-91
-00:03:21,070 --> 00:03:22,810
 I don't want anybody to be coauthors.
 
 92
@@ -394,15 +271,9 @@ So I just be myself.
 It's just gonna be my paper.
 
 94
-00:03:25,460 --> 00:03:26,860
+00:03:25,460 --> 00:03:29,940
 And we all just eat it,
-
-95
-00:03:26,860 --> 00:03:27,910
 just swallow it,
-
-96
-00:03:27,910 --> 00:03:29,940
 and say, okay, that's the way it is.
 
 97
@@ -410,38 +281,23 @@ and say, okay, that's the way it is.
 That's the second approach.
 
 98
-00:03:31,220 --> 00:03:34,028
+00:03:31,220 --> 00:03:40,610
 And the approach number
 three, which I use,
-
-99
-00:03:34,028 --> 00:03:37,770
 which I see more and more
 often is the approach
-
-100
-00:03:37,770 --> 00:03:40,610
 where the decision is made ad hoc.
 
 101
-00:03:40,610 --> 00:03:42,960
+00:03:40,610 --> 00:03:46,310
 So we just, we just make a decision
-
-102
-00:03:42,960 --> 00:03:46,310
 with no particular rule behind it.
 
 103
-00:03:46,310 --> 00:03:47,620
+00:03:46,310 --> 00:03:54,980
 It's just, yeah, these three people,
-
-104
-00:03:47,620 --> 00:03:50,170
 because they were working
 more on this paper,
-
-105
-00:03:50,170 --> 00:03:54,980
 just because, so decisions
 is made by somehow.
 
@@ -450,17 +306,11 @@ is made by somehow.
 There's no clear explanation.
 
 107
-00:03:56,600 --> 00:03:59,670
+00:03:56,600 --> 00:04:03,950
 There's no real set of
 rules written on the wall,
-
-108
-00:03:59,670 --> 00:04:01,570
 and anybody can just
 look at it and understand
-
-109
-00:04:01,570 --> 00:04:03,950
 okay that's the rule we use in our team.
 
 110
@@ -474,26 +324,17 @@ The first approach, the
 second one or the third one?
 
 112
-00:04:09,060 --> 00:04:11,020
+00:04:09,060 --> 00:04:15,910
 So probably if you are
 subscribed to my channel
-
-113
-00:04:11,020 --> 00:04:13,220
 then you will probably cross
 out the option number three
-
-114
-00:04:13,220 --> 00:04:15,910
 because we are all, you
 know, we all like rules.
 
 115
-00:04:15,910 --> 00:04:18,709
+00:04:15,910 --> 00:04:22,129
 I mean, I am trying to tell you this
-
-116
-00:04:18,709 --> 00:04:22,129
 for many many years on
 my blog and my videos.
 
@@ -532,20 +373,14 @@ It is supposed to not offend anybody.
 Of course, it may offend.
 
 125
-00:04:40,560 --> 00:04:43,550
+00:04:40,560 --> 00:04:44,960
 For example, the person
 just quits the group,
-
-126
-00:04:43,550 --> 00:04:44,960
 and we still publish the paper.
 
 127
-00:04:44,960 --> 00:04:46,440
+00:04:44,960 --> 00:04:49,397
 So shall we include the person
-
-128
-00:04:46,440 --> 00:04:49,397
 who was working with us two
 years ago on this project.
 
@@ -555,16 +390,10 @@ And she quit two years ago
 and went to work for Google.
 
 130
-00:04:53,180 --> 00:04:55,640
+00:04:53,180 --> 00:05:00,030
 Now she's in Google,
 but we continue writing,
-
-131
-00:04:55,640 --> 00:04:57,560
 continue working on that, continue writing
-
-132
-00:04:57,560 --> 00:05:00,030
 those papers we started two years ago.
 
 133
@@ -577,32 +406,17 @@ person as a coauthor?
 That's a tricky question, hard to answer.
 
 135
-00:05:05,200 --> 00:05:06,950
+00:05:05,200 --> 00:05:09,340
 But if the answer will be, of course not,
-
-136
-00:05:06,950 --> 00:05:09,340
 then it will demotivate
 people who stay with us.
 
 137
-00:05:09,340 --> 00:05:11,040
+00:05:09,340 --> 00:05:18,840
 Now they will understand that
-
-138
-00:05:11,040 --> 00:05:13,680
 no matter how much they invest right now,
-
-139
-00:05:13,680 --> 00:05:14,810
 the time and efforts,
-
-140
-00:05:14,810 --> 00:05:16,460
 they will be most probably excluded
-
-141
-00:05:16,460 --> 00:05:18,840
 if they decide to quit in sometime.
 
 142
@@ -631,58 +445,25 @@ And the second option,
 there are also benefits.
 
 148
-00:05:31,130 --> 00:05:32,350
+00:05:31,130 --> 00:05:54,260
 So if you,
-
-149
-00:05:32,350 --> 00:05:34,474
 if you let the primary author
-
-150
-00:05:34,474 --> 00:05:36,710
 be the decision maker,
-
-151
-00:05:36,710 --> 00:05:39,895
 then it gives extra motivation,
 additional motivation
-
-152
-00:05:39,895 --> 00:05:41,490
 for that person
-
-153
-00:05:41,490 --> 00:05:43,260
 to stand out
-
-154
-00:05:43,260 --> 00:05:46,270
 and to demonstrate their
 achievements much better
-
-155
-00:05:46,270 --> 00:05:48,390
 than being just
-
-156
-00:05:48,390 --> 00:05:51,320
 inside a list of coauthors
 among everybody else
-
-157
-00:05:51,320 --> 00:05:54,260
 who maybe not work as good as that person.
 
 158
-00:05:54,260 --> 00:05:56,600
+00:05:54,260 --> 00:06:00,370
 Because you know, in a team of 10 people
-
-159
-00:05:56,600 --> 00:05:57,840
 it's impossible to imagine
-
-160
-00:05:57,840 --> 00:06:00,370
 that all 10 people they
 contribute equally.
 
@@ -691,61 +472,34 @@ contribute equally.
 Sometimes there will be lazy people.
 
 162
-00:06:02,090 --> 00:06:04,380
+00:06:02,090 --> 00:06:06,740
 Sometimes there will be
 people who don't work
-
-163
-00:06:04,380 --> 00:06:06,740
 because they don't like to work. Whatever.
 
 164
-00:06:06,740 --> 00:06:10,030
+00:06:06,740 --> 00:06:12,810
 There could be many
 reasons for having people
-
-165
-00:06:10,030 --> 00:06:12,810
 who contribute much
 less than somebody else.
 
 166
-00:06:12,810 --> 00:06:15,160
+00:06:12,810 --> 00:06:20,570
 So that's somebody else
 who's a top contributor,
-
-167
-00:06:15,160 --> 00:06:17,440
 who works day and night on this paper,
-
-168
-00:06:17,440 --> 00:06:20,570
 who puts a lot of effort into it.
 
 169
-00:06:20,570 --> 00:06:22,195
+00:06:20,570 --> 00:06:37,420
 He or she will not be happy to see
-
-170
-00:06:22,195 --> 00:06:25,110
 that after the paper is published
-
-171
-00:06:25,110 --> 00:06:28,400
 his name or her name stays
 on the position number five
-
-172
-00:06:28,400 --> 00:06:32,690
 among other 10 guys who,
 most of who, or some of who,
-
-173
-00:06:32,690 --> 00:06:34,470
 barely touched the keyboard
-
-174
-00:06:34,470 --> 00:06:37,420
 during the lifetime of the project.
 
 175
@@ -753,23 +507,14 @@ during the lifetime of the project.
 So that's a negative side.
 
 176
-00:06:39,480 --> 00:06:42,740
+00:06:39,480 --> 00:06:48,600
 It may demotivate that key performers,
-
-177
-00:06:42,740 --> 00:06:46,040
 if we apply the the principle
-
-178
-00:06:46,040 --> 00:06:48,600
 from the first scenario.
 
 179
-00:06:48,600 --> 00:06:50,430
+00:06:48,600 --> 00:06:53,330
 So in this case, the second scenario
-
-180
-00:06:50,430 --> 00:06:53,330
 is kind of gives advantages
 to top performers.
 
@@ -782,20 +527,11 @@ It just says, you'll be the author.
 You'll be the paper publisher.
 
 183
-00:06:57,620 --> 00:06:59,900
+00:06:57,620 --> 00:07:07,570
 So everybody else, they
 will try to help you
-
-184
-00:06:59,900 --> 00:07:03,067
 in order to motivate you to involve,
-
-185
-00:07:03,067 --> 00:07:04,700
 to include me,
-
-186
-00:07:04,700 --> 00:07:07,570
 include them into the list of co-authors.
 
 187
@@ -803,17 +539,11 @@ include them into the list of co-authors.
 For example, I'm in the team.
 
 188
-00:07:09,120 --> 00:07:12,040
+00:07:09,120 --> 00:07:18,780
 I see this guy he's working
 hard to publish a paper
-
-189
-00:07:12,040 --> 00:07:14,165
 and I see that the paper
 potentially could be,
-
-190
-00:07:14,165 --> 00:07:18,780
 could be publishable and
 could be really good paper.
 
@@ -826,15 +556,9 @@ I will try to help that person.
 I will try to work with them.
 
 193
-00:07:21,800 --> 00:07:23,630
+00:07:21,800 --> 00:07:26,680
 I will try to say, how can I help you,
-
-194
-00:07:23,630 --> 00:07:24,890
 maybe invite me for the meeting,
-
-195
-00:07:24,890 --> 00:07:26,680
 invite me for the discussion.
 
 196
@@ -842,23 +566,13 @@ invite me for the discussion.
 Let me write something for you.
 
 197
-00:07:27,930 --> 00:07:29,980
+00:07:27,930 --> 00:07:31,990
 Maybe write you some software,
-
-198
-00:07:29,980 --> 00:07:31,990
 so I will try to help.
 
 199
-00:07:31,990 --> 00:07:33,470
-But
-
-200
-00:07:33,470 --> 00:07:34,680
-there are negative sides
-
-201
-00:07:34,680 --> 00:07:37,080
+00:07:31,990 --> 00:07:37,080
+But there are negative sides
 and positive sides in both options.
 
 202
@@ -866,15 +580,9 @@ and positive sides in both options.
 So I'm asking you, what do you think?
 
 203
-00:07:38,390 --> 00:07:39,950
+00:07:38,390 --> 00:07:42,640
 Maybe you can share your experience
-
-204
-00:07:39,950 --> 00:07:41,110
 how it works in your team,
-
-205
-00:07:41,110 --> 00:07:42,640
 how it works in your group.
 
 206
@@ -890,11 +598,8 @@ What's the rule?
 What's the principle of the authorship?
 
 209
-00:07:46,710 --> 00:07:48,790
+00:07:46,710 --> 00:07:52,980
 So who gets the privilege of
-
-210
-00:07:48,790 --> 00:07:52,980
 being authors of the papers
 your group is publishing?
 
@@ -913,5 +618,4 @@ Stay tuned.
 214
 00:07:55,843 --> 00:07:56,676
 Bye-bye.
-
 `
